9bf9bacecb0acf60629ad76c45d77d7eab8f8a23
oscar.x.sanchez@oracle.com <oscar.x.sanchez@oracle.com>
2019-07-23 15:27:03 +0000

    fdsf

--- VARS/vars.sh
+++ VARS/vars.sh
@@ -2,7 +2,7 @@
 export TF_VAR_tenancy_ocid="ocid1.tenancy.oc1..aaaaaaaaif5jkjgrzztb7vj4zonc7kvynba3zooyw3lzgtaj57idl5js2rca"
 export TF_VAR_user_ocid="ocid1.user.oc1..aaaaaaaa7dpdau7iq4oq3bfrqkaxf6rldwv4xu2gkmnl4344h7vyydjo6ima"
 export TF_VAR_api_fingerprint="77:36:b2:3b:6e:3b:45:0e:fa:e7:9c:ea:3d:0e:50:f0"
-export TF_VAR_api_private_key_path="./KEYS/wedosinkey.pem"
+export TF_VAR_api_private_key_path="ocikey"
 
 ### Region
 export TF_VAR_region="eu-frankfurt-1"
