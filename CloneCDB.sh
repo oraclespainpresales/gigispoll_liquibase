028ab6734aeba0517e404d91bdf52b4376c9a434
oscar.x.sanchez@oracle.com <oscar.x.sanchez@oracle.com>
2019-07-23 15:18:13 +0000

    fsdf

--- CloneCDB.sh
+++ CloneCDB.sh
@@ -3,9 +3,9 @@
 . ./VARS/vars.sh
 
 
-terraform init Terraform
-terraform plan Terraform
-terraform apply -auto-approve Terraform
+terraform init Terraform/createdb.tf 
+terraform plan Terraform/createdb.tf 
+terraform apply -auto-approve Terraform/createdb.tf 
 terraform output -json instance_ips | jq '.value[0]' > ip_$TF_VAR_demozone.json
 mkdir -p Demozones/$TF_VAR_demozone
 mv terraform.tfstate Demozones/$TF_VAR_demozone
