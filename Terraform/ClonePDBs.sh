b903f6247f7fcac9d69ab3c5ac9f0fb0cc52f8f5
oscar.x.sanchez@oracle.com <oscar.x.sanchez@oracle.com>
2019-08-28 16:04:59 +0000

    das

--- Terraform/ClonePDBs.sh
+++ Terraform/ClonePDBs.sh
@@ -2,22 +2,12 @@
 
 echo $1
 
-export ORACLE_SID=DB$1
-export ORACLE_BASE=/u01/app/oracle
-export ORACLE_HOME=/u01/app/oracle/product/18.0.0.0/dbhome_1
-
-LOWERHOST=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-
-export ORACLE_HOST=hostdb$LOWERHOST
-
-env | grep ORACLE
-
 sudo cp ClonePDBs.sql /home/oracle
+sudo cp execDB.sh /home/oracle
 sudo chown oracle:oinstall /home/oracle/ClonePDBs.sql
-sudo su -m -l oracle -c "sqlplus -S / as sysdba @ClonePDBs.sql"
+sudo chown oracle:oinstall /home/oracle/execDB.sh
+sudo chmod +x /home/oracle/execDB.sh
 
-sudo su -m -l oracle -c "impdp system/"Welcome1#Oc4j1013-"@$ORACLE_HOST:1521/dodbhp_pdb1.sub03010825490.devopsvcn.oraclevcn.com directory=DATA_PUMP_DIR logfile=microservice.log schemas=MICROSERVICE   network_link=clone_link_sql"
-
-sudo su -m -l oracle -c "impdp system/"Welcome1#Oc4j1013-"@$ORACLE_HOST:1521/pdbjson.sub03010825490.devopsvcn.oraclevcn.com directory=DATA_PUMP_DIR logfile=microservice.log schemas=MICROSERVICE network_link=clone_link_json"
+sudo su -l oracle -c "/home/oracle/execDB.sh $1"
 
 sudo rm /home/oracle/ClonePDBs.sql
