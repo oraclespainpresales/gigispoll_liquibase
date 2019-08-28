4932fe2cde7519240acc8563a9a88614ffe3a38d
oscar.x.sanchez@oracle.com <oscar.x.sanchez@oracle.com>
2019-08-28 09:57:21 +0000

    dasd

--- CloneCDB.sh
+++ CloneCDB.sh
@@ -6,10 +6,8 @@
 terraform init Terraform
 terraform plan Terraform
 terraform apply -auto-approve Terraform
-terraform output -json instance_ips | jq '.value[0]' > ip_$TF_VAR_demozone.json
 mkdir -p Demozones/$TF_VAR_demozone
 mv terraform.tfstate Demozones/$TF_VAR_demozone
-mv ip_$TF_VAR_demozone.json Demozones/$TF_VAR_demozone
 git add  Demozones/$TF_VAR_demozone
 git commit -m "Developer Cloud Service Terraform Deploy $(Date) $TF_VAR_demozone"
 git push -u origin master
