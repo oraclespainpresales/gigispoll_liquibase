5687b35461de1a5ad54477fb0f6947276e03faf6
manuela.mustatea@oracle.com <manuela.mustatea@oracle.com>
2020-07-07 09:55:28 +0000

    Rename script_01.sql to script.sql

---
+++ sqlScripts/script.sql
@@ -0,0 +1,5 @@
+CREATE TABLE tab1 (
+  id          NUMBER,
+  description VARCHAR2(50),
+  CONSTRAINT tab1_pk PRIMARY KEY (id)
+);
--- sqlScripts/script_01.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE tab1 (
-  id          NUMBER,
-  description VARCHAR2(50),
-  CONSTRAINT tab1_pk PRIMARY KEY (id)
-);
-CREATE SEQUENCE tab1_seq;
-
-INSERT INTO tab1 (id, description) VALUES (tab1_seq.NEXTVAL, 'Description for ' || tab1_seq.CURRVAL);
-COMMIT;
