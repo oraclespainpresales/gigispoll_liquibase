fe6a74ec7bd9a4061a4ece9510ac78cc4b5b6833
Iván Sampedro <ivan.sampedro@oracle.com>
2020-11-26 14:56:39 +0000

    Update script_uuidtable.sql

--- sqlScripts/script_uuidtable.sql
+++ sqlScripts/script_uuidtable.sql
@@ -3,9 +3,10 @@
 --------------------------------------------------------
 BEGIN
     BEGIN
-        execute immediate 'drop table MICROSERVICE.POLLUUID purge';
+        execute immediate 'drop table MICROSERVICE.POLLUUID';
         EXCEPTION WHEN OTHERS THEN NULL;
     END;
+    
     execute immediate 'CREATE TABLE MICROSERVICE.POLLUUID ( 
         UUID VARCHAR2(40 BYTE) NOT NULL, 
         ORDERID VARCHAR2(20 BYTE) NOT NULL, 
