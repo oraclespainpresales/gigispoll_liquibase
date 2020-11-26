680f54add58045f3d969bad440363b597b481edb
Iván Sampedro <ivan.sampedro@oracle.com>
2020-11-26 15:30:08 +0000

    Update script_uuidtable.sql

--- sqlScripts/script_uuidtable.sql
+++ sqlScripts/script_uuidtable.sql
@@ -3,7 +3,7 @@
 --------------------------------------------------------
 BEGIN
     BEGIN
-        execute immediate 'drop table MICROSERVICE.POLLUUID;'
+        execute immediate 'drop table MICROSERVICE.POLLUUID;';
         EXCEPTION WHEN OTHERS THEN NULL;
     END;
     
