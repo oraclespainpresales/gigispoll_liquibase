5c6693c4c3ba1de55a6e3a75a715cfff395afde5
Iván Sampedro <ivan.sampedro@oracle.com>
2020-11-26 15:38:02 +0000

    Update script_uuidtable.sql

--- sqlScripts/script_uuidtable.sql
+++ sqlScripts/script_uuidtable.sql
@@ -14,11 +14,15 @@ BEGIN
         USED VARCHAR2(1 BYTE) DEFAULT ''N''
     )';
     
-    execute immediate 'ALTER TABLE MICROSERVICE.PAYMENTS 
-    ADD ( 
-        SERVICESURVEY NUMBER DEFAULT 0,
-        SERVICESCOMMENTS VARCHAR2(2000 BYTE)
-    )';
+    BEGIN
+        execute immediate 'ALTER TABLE MICROSERVICE.PAYMENTS 
+        ADD ( 
+            SERVICESURVEY NUMBER DEFAULT 0,
+            SERVICESCOMMENTS VARCHAR2(2000 BYTE)
+        )';
+        
+        EXCEPTION WHEN OTHERS THEN NULL;
+    END;
     
     COMMIT;
 END;
